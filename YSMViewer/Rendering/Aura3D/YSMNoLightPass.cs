8a034758e826051a227c69804b50a050934d5aa3
Dr.Abc <me@drabc.net>
2026-06-08 00:56:10 +0800

    fix: embed complete custom shaders with normal-based shading
    Replace fragile string-patching approach with full custom vertex/fragment
    shaders that include vNormal output and simple directional lighting.
    - Light direction: normalize(1,1,1) from upper-right-front
    - shade = 0.2 + 0.8 * diff (strong contrast)
    - Default intensity 0.5

--- YSMViewer/Rendering/Aura3D/YSMNoLightPass.cs
+++ YSMViewer/Rendering/Aura3D/YSMNoLightPass.cs
@@ -15,7 +15,7 @@ public class YSMNoLightPass : NoLightPass
     private bool _defaultBaseColorUploaded;
 
     /// <summary>0 = off, >0 = simple shading intensity</summary>
-    public float SimpleShadingIntensity { get; set; } = 0.3f;
+    public float SimpleShadingIntensity { get; set; } = 0.5f;
 
     public YSMNoLightPass(RenderPipeline renderPipeline) : base(renderPipeline)
     {
@@ -25,25 +25,104 @@ public class YSMNoLightPass : NoLightPass
             "precision mediump float;",
             "precision mediump float;\n//{{defines}}");
 
-        VertexShader = ShaderResource.NoLightVert
-            .Replace("out vec2 vTexCoord;", "out vec2 vTexCoord;\nout vec3 vNormal;")
-            .Replace(
-                "\tgl_Position = projectionMatrix * viewMatrix * worldPosition;",
-                "\tvNormal = normalize(mat3(normalMatrix) * normal);\n\tgl_Position = projectionMatrix * viewMatrix * worldPosition;");
-
-        FragmentShader = FragmentShader
-            .Replace("in vec2 vTexCoord;", "in vec2 vTexCoord;\nin vec3 vNormal;")
-            .Replace(
-                "uniform float alphaCutoff;",
-                "uniform float alphaCutoff;\nuniform float simpleShadingIntensity;")
-            .Replace(
-                "\toutColor = baseColor;",
-@"    vec3 lightDir = normalize(vec3(1.0, 1.0, 1.0));
-    float diff = max(dot(normalize(vNormal), lightDir), 0.0);
-    float shade = 0.5 + 0.5 * diff;
-    baseColor.rgb = mix(baseColor.rgb, baseColor.rgb * shade, simpleShadingIntensity);
-    outColor = baseColor;");
+        VertexShader = _VertexShader;
+        FragmentShader = _FragmentShader;
     }
+
+private const string _VertexShader = @"#version 300 es
+precision mediump float;
+
+#define BONE_NUMBER 150
+
+//{{defines}}
+
+layout(location = 0) in vec3 position;
+layout(location = 1) in vec2 texCoord;
+layout(location = 2) in vec4 color;
+layout(location = 3) in vec3 normal;
+layout(location = 4) in vec3 tangent;
+layout(location = 5) in vec3 bitangent;
+layout(location = 6) in vec4 boneIndices;
+layout(location = 7) in vec4 boneWeights;
+
+#ifdef INSTANCED_MESH
+layout(location = 8) in mat4 modelMatrix;
+layout(location = 12) in mat4 normalMatrix;
+#endif
+
+#ifdef SKINNED_MESH
+uniform mat4 BoneMatrices[BONE_NUMBER];
+#endif
+
+#ifndef INSTANCED_MESH
+uniform mat4 modelMatrix;
+uniform mat4 normalMatrix;
+#endif
+
+uniform mat4 viewMatrix;
+uniform mat4 projectionMatrix;
+
+out vec2 vTexCoord;
+out vec3 vNormal;
+
+void main()
+{
+	vTexCoord = texCoord;
+
+#ifdef SKINNED_MESH
+	
+	int idx0 = clamp(int(boneIndices.x), 0, BONE_NUMBER - 1);
+    int idx1 = clamp(int(boneIndices.y), 0, BONE_NUMBER - 1);
+    int idx2 = clamp(int(boneIndices.z), 0, BONE_NUMBER - 1);
+    int idx3 = clamp(int(boneIndices.w), 0, BONE_NUMBER - 1);
+
+	float sum = boneWeights.x + boneWeights.y + boneWeights.z + boneWeights.w;
+    vec4 w = (sum > 0.0001) ? boneWeights / sum : vec4(1.0, 0.0, 0.0, 0.0);
+
+	mat4 skinMatrix = w.x * BoneMatrices[idx0];
+    skinMatrix      += w.y * BoneMatrices[idx1];
+    skinMatrix      += w.z * BoneMatrices[idx2];
+    skinMatrix      += w.w * BoneMatrices[idx3];
+
+	vec4 worldPosition = modelMatrix * skinMatrix * vec4(position, 1.0);
+
+#else
+	vec4 worldPosition = modelMatrix * vec4(position, 1.0);
+#endif
+
+	vNormal = normalize(mat3(normalMatrix) * normal);
+	gl_Position = projectionMatrix * viewMatrix * worldPosition;
+}
+";
+
+    private const string _FragmentShader = @"#version 300 es
+precision mediump float;
+//{{defines}}
+out vec4 outColor;
+
+in vec2 vTexCoord;
+in vec3 vNormal;
+
+uniform sampler2D BaseColorTexture;
+uniform float alphaCutoff;
+uniform float simpleShadingIntensity;
+
+void main()
+{
+	vec4 baseColor = texture(BaseColorTexture, vTexCoord);
+
+#if defined(BLENDMODE_MASKED) || defined(BLENDMODE_TRANSLUCENT)
+	if (baseColor.a <= alphaCutoff)
+		discard;
+#endif
+
+	vec3 lightDir = normalize(vec3(1.0, 1.0, 1.0));
+	float diff = max(dot(normalize(vNormal), lightDir), 0.0);
+	float shade = 0.2 + 0.8 * diff;
+	baseColor.rgb = mix(baseColor.rgb, baseColor.rgb * shade, simpleShadingIntensity);
+	outColor = baseColor;
+}
+";
 
     public override void Setup()
     {
